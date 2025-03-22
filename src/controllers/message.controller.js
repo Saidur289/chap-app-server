9448891c143cefdad9858d7d4b35640d2252e706
Saidur289 <saidur.riaz1@gmailcomm>
2025-03-23 00:25:57 +0600

    create function for live message

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/clodinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -29,5 +30,34 @@ export const getMessages = async(req, res) => {
     catch(error){
         console.log('error cought in get Messages function', error);
         res.status(500).json('Server Error')
+    }
+}
+export const sendMessage = async(req, res) => {
+    try {
+        const {text, image} = req.body 
+        const {id: receiverId} = req.params
+        console.log('receiverId', receiverId);
+        const senderId = req.user?._id 
+        console.log(senderId);
+        let imageURL ;
+        if(image){
+            // upload base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageURL = uploadResponse.secure_url
+            console.log('url of image', imageURL);
+
+        }
+        // after validation save new message from sender and receviver
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageURL
+        })
+        await newMessage.save()
+        // todo: realtime functionality goes here by socket io
+    } catch (error) {
+        console.log("Error in sendMessage controller", error.message);
+        res.status(500).json({message: "server Internal Error"})
     }
 }
