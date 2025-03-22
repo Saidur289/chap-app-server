df273a0724fdb26180f72ad6f27e6e1ee8d2cb5a
Saidur289 <saidur.riaz1@gmailcomm>
2025-03-22 22:20:01 +0600

    set up cloadinary in my project

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/clodinary.js";
 import { generateToken } from "../lib/utilis.js";
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs'
@@ -80,5 +81,25 @@ export const logout = (req, res) => {
     }
 }
 export const updateProfile = async(req, res) => {
-    
+    try {
+        const {profilePic} = req.body
+        const userId= req.user._id 
+        if(!profilePic){
+            return res.status(400).json({message: 'profile pic is required'})
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true})
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        console.log('Update profile error', error);
+    }
+}
+export const checkAuth = (req, res) => {
+    try {
+      res.status(200).json(req.user)  
+        
+    } catch (error) {
+        console.log('error in checkauth route', );
+        res.status(500).json({message: 'Internal Server Error'})
+    }
 }
